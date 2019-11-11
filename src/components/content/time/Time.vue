7e96426b4eb87624bc22b9ca4ab7017d6fd1bf91
zhangzhixing <zhangzhixing@boce.cn>
2019-11-11 11:52:21 +0800

    时间页面调整

--- src/components/content/time/Time.vue
+++ src/components/content/time/Time.vue
@@ -1,16 +1,16 @@
 <template>
     <div id="time">
         <el-row>
-            <el-col :span="1">时间戳（秒）</el-col>
-            <el-col :span="3">
+            <el-col :span="2">时间戳（秒）</el-col>
+            <el-col :span="4">
                 <el-input v-model="timestamp" placeholder="时间戳（秒）"></el-input>
             </el-col>
-            <el-col :span="4">
+            <el-col :span="6">
                 <el-button type="primary" @click="changeTime('timeStampToTime')">时间戳 转 时间</el-button>
                 <el-button type="primary" @click="changeTime('timeToTimeStamp')">时间  转 时间戳</el-button>
             </el-col>
-            <el-col :span="1">北京时间</el-col>
-            <el-col :span="3">
+            <el-col :span="2">北京时间</el-col>
+            <el-col :span="4">
                 <el-date-picker
                         v-model="date"
                         :default-value="new Date()"
